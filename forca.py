0dcee77ffab32e38f845d43642fcbc20e829ba2f
Fernanda Amaral de Souza <fernanda_amaral@Decosdh.com>
2023-02-10 07:57:24 -0300

    for para ler a palavra

--- forca.py
+++ forca.py
@@ -1,25 +1,38 @@
 def jogar_forca():
 
-    palavra_secreta  = "banana"
+    palavra_secreta  = "banana".upper()
+    letras_corretas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_corretas)
 
     while(not enforcou and not acertou):
 
         chute = input('Qual letra? ')
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
+    if(chute in palavra_secreta):
         index = 0
-
         for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+            if(chute == letra):
+                letras_corretas[index] = letra
             index = index + 1
+    else:
+        erros += 1
 
-        print('JOGANDO...')
+    enforcou = erros == 6
+    acertou = " " not in letras_corretas
+    print(letras_corretas)
 
-    print('FIM DO JOGO!')
+    if(acertou):
+        print('Você ganhou!')
+    else:
+        print('Você perdeu!')
+    print('Fim do jogo!')
 
-if (__name__  == "__main__"):
+if (__name__ == "__main__"):
     jogar_forca()
+
